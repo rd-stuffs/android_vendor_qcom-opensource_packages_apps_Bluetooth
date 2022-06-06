640d10922dc30b4266f4ee4237f34e1b02c508d8
Shashank Dash <quic_vendetta@quicinc.com>
2022-06-06 16:27:07 +0530

    ADM: Ignore updating of mHfpActiveDevice incase of CSIP.
    - Overwriting mHfpActiveDevice with last connected absolute address.
      Fixed it by Ignoring updating of mHfpActiveDevice incase of CSIP.
    
    Change-Id: If203597efdad258b00524a08bfce693d07cdad59

--- src/com/android/bluetooth/btservice/ActiveDeviceManager.java
+++ src/com/android/bluetooth/btservice/ActiveDeviceManager.java
@@ -708,6 +708,12 @@ public class ActiveDeviceManager {
         if (!headsetService.setActiveDevice(device)) {
             return false;
         }
+        if ((ApmConstIntf.getQtiLeAudioEnabled() ||
+             ApmConstIntf.getAospLeaEnabled()) &&
+             mAdapterService.isGroupDevice(device)) {
+            Log.d(TAG, "setHfpActiveDevice(" + device + ")" + "is a group device, ignore");
+            return true;
+        }
         mHfpActiveDevice = device;
         return true;
     }
