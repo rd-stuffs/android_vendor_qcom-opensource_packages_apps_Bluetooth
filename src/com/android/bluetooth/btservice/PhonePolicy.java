704e4dc93ff01c81350a073978f0e9983e90747d
Akash Srivastava <akashniki@gmail.com>
2022-08-13 07:18:12 +0000

    Revert "BC: Auto connect to all group members"
    This reverts commit de7374a5d9291d7ad856ec57da053139c7dedac0.
    
    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth/src/com/android/bluetooth/btservice/PhonePolicy.java:45: error: package com.android.bluetooth.groupclient does not exist
    import com.android.bluetooth.groupclient.GroupService;
                                            ^
    1 error
    
    * Well... com.android.bluetooth.groupclient.GroupService exists in BSP but isn't public. This is the first time they used import instead of calling/accessing it via ReflectionUtils.getRequiredClass
    
    Change-Id: I0383f5c2555aeacdaf73ce60cbbc02eb087de4fc

--- src/com/android/bluetooth/btservice/PhonePolicy.java
+++ src/com/android/bluetooth/btservice/PhonePolicy.java
@@ -42,8 +42,6 @@ import com.android.bluetooth.a2dpsink.A2dpSinkService;
 import com.android.bluetooth.apm.ApmConstIntf;
 import com.android.bluetooth.apm.MediaAudioIntf;
 import com.android.bluetooth.apm.CallAudioIntf;
-import com.android.bluetooth.groupclient.GroupService;
-import android.bluetooth.DeviceGroup;
 import com.android.bluetooth.btservice.InteropUtil;
 import com.android.bluetooth.btservice.storage.DatabaseManager;
 import com.android.bluetooth.hearingaid.HearingAidService;
@@ -61,7 +59,6 @@ import java.lang.reflect.*;
 
 import java.util.HashSet;
 import java.util.List;
-import java.util.ArrayList;
 import java.util.Objects;
 
 // Describes the phone policy
@@ -780,28 +777,8 @@ class PhonePolicy {
             }
             final BluetoothDevice mostRecentlyActiveA2dpDevice =
                 mDatabaseManager.getMostRecentlyConnectedA2dpDevice();
-            if (Objects.equals(mostRecentlyActiveA2dpDevice, device)) {
-                GroupService setCoordinator = GroupService.getGroupService();
-                List<BluetoothDevice> listOfDevices = new ArrayList<BluetoothDevice>();
-                if (setCoordinator != null) {
-                    int setId = setCoordinator.getRemoteDeviceGroupId(device, null);
-                    DeviceGroup devGrp = setCoordinator.getCoordinatedSet(setId);
-                    if (devGrp != null) {
-                        listOfDevices = devGrp.getDeviceGroupMembers();
-                    } else {
-                        Log.d(TAG, "Failed to get dev Group instance");
-                        listOfDevices.add(device);
-                    }
-                } else {
-                    Log.d(TAG, "Fail to get CSIP instance");
-                    listOfDevices.add(device);
-                }
-
-                for (BluetoothDevice dev : listOfDevices) {
-                    Log.d(TAG, "connectBC : " + dev);
-                    connectBC(dev);
-                }
-            }
+            if (Objects.equals(mostRecentlyActiveA2dpDevice, device))
+                connectBC(device);
         }
     }
     private void connectBC(BluetoothDevice device) {
