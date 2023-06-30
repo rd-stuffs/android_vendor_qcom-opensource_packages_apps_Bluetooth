4dfd9f39c01a0292b4293f28a9c1f6658fa97829
pramod kotreshappa <quic_kpramod@quicinc.com>
2023-06-30 10:27:52 -0700

    Fix for multiple legacy devices shown as active
    Do not populated LE-A active devices if LE-A is not connected
    
    Change-Id: I900236d290ef204abdbaa4d528b4285e550d8c3a

--- src/com/android/bluetooth/le_audio/LeAudioService.java
+++ src/com/android/bluetooth/le_audio/LeAudioService.java
@@ -1278,11 +1278,21 @@ public class LeAudioService extends ProfileService {
 
         ActiveDeviceManagerServiceIntf activeDeviceManager =
                                             ActiveDeviceManagerServiceIntf.get();
-        mActiveAudioOutDevice =
+        BluetoothDevice outDevice = activeDeviceManager.getActiveAbsoluteDevice(ApmConstIntf.AudioFeatures.MEDIA_AUDIO);
+        BluetoothDevice inDevice = activeDeviceManager.getActiveAbsoluteDevice(ApmConstIntf.AudioFeatures.CALL_AUDIO);
+        mActiveAudioOutDevice = null;
+        mActiveAudioInDevice = null;
+        if (outDevice != null &&
+            getConnectionState(outDevice) ==  BluetoothProfile.STATE_CONNECTED)
+            mActiveAudioOutDevice = outDevice;
+        if (inDevice != null &&
+            getConnectionState(inDevice) == BluetoothProfile.STATE_CONNECTED)
+            mActiveAudioInDevice = inDevice;
+/*        mActiveAudioOutDevice =
             activeDeviceManager.getActiveAbsoluteDevice(ApmConstIntf.AudioFeatures.MEDIA_AUDIO);
         mActiveAudioInDevice =
             activeDeviceManager.getActiveAbsoluteDevice(ApmConstIntf.AudioFeatures.CALL_AUDIO);
-
+*/
         int ActiveAudioMediaProfile =
             activeDeviceManager.getActiveProfile(ApmConstIntf.AudioFeatures.MEDIA_AUDIO);
         int ActiveAudioCallProfile =
