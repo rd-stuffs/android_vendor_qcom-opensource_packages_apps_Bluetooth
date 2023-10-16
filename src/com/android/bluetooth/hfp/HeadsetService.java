9f24283a8400dc34ec55d9b18e58a053257e016d
Pramod Sivaraman <quic_pramods@quicinc.com>
2023-10-16 16:45:07 +0530

    Battery Indicator changes to pass HFP/AG/PSI/BV-03-C and HFP/AG/IIA/BV-01-I
    After making and flashing build with the change,
    1. adb root && adb wait-for-device && adb remount
    2. adb shell am broadcast --ei "android.bluetooth.extra.battery.STATE"
     <battery ind value> -a "android.bluetooth.action.BATTERY_STATE_CHANGED"
    
    <battery Ind value> takes value from 0-5
    Change-Id: I7f05b907935335d507b8bf4f49c962aa7937a05c

--- src/com/android/bluetooth/hfp/HeadsetService.java
+++ src/com/android/bluetooth/hfp/HeadsetService.java
@@ -193,6 +193,8 @@ public class HeadsetService extends ProfileService {
     private Context mContext = null;
     private AudioServerStateCallback mServerStateCallback = new AudioServerStateCallback();
     private static final int AUDIO_CONNECTION_DELAY_DEFAULT = 100;
+    private static final String ACTION_BATTERY_STATE_CHANGED = "android.bluetooth.action.BATTERY_STATE_CHANGED";
+    private static final String EXTRA_BATTERY_STATE = "android.bluetooth.extra.battery.STATE";
 
     @Override
     public IProfileServiceBinder initBinder() {
@@ -279,7 +281,8 @@ public class HeadsetService extends ProfileService {
         filter.addAction(BluetoothDevice.ACTION_BOND_STATE_CHANGED);
         filter.addAction(BluetoothA2dp.ACTION_PLAYING_STATE_CHANGED);
         filter.addAction(BluetoothA2dp.ACTION_CONNECTION_STATE_CHANGED);
-        registerReceiver(mHeadsetReceiver, filter);
+        filter.addAction(ACTION_BATTERY_STATE_CHANGED);
+        registerReceiver(mHeadsetReceiver, filter, Context.RECEIVER_EXPORTED);
         // Step 7: Mark service as started
 
         setHeadsetService(this);
@@ -617,6 +620,12 @@ public class HeadsetService extends ProfileService {
                     logD("Received BluetoothA2dp Connection State changed");
                     mHfpA2dpSyncInterface.updateA2DPConnectionState(intent);
                     break;
+                }
+                case ACTION_BATTERY_STATE_CHANGED: {
+                     logD("Received ACTION_BATTERY_STATE_CHANGED");
+                     int cindBatteryLevel = intent.getIntExtra(EXTRA_BATTERY_STATE, 0);
+                     mSystemInterface.getHeadsetPhoneState().setCindBatteryCharge(cindBatteryLevel);
+                     break;
                 }
                 default:
                     Log.w(TAG, "Unknown action " + action);
