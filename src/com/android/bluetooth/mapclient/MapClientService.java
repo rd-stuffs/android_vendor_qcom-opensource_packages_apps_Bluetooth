CodeLinaro-mirror/la_platform_vendor_qcom-opensource_packages_apps_Bluetooth

Send Image from MapClient API
- New API is added to send Image via MapClient

- Debug logs are enabled

CRs-Fixed: 3320345
Change-Id: Ib2bba2f44976d9db967686751e31143fe44a2352

src/com/android/bluetooth/mapclient/MapClientService.java
@@ -13,6 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ /*
+ * Changes from Qualcomm Innovation Center are provided under the following license:
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ *
+*/
 
 package com.android.bluetooth.mapclient;
 
@@ -52,7 +58,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class MapClientService extends ProfileService {
     private static final String TAG = "MapClientService";
 
-    static final boolean DBG = false;
+    static final boolean DBG = true;
     static final boolean VDBG = false;
 
     static final int MAXIMUM_CONNECTED_DEVICES = 4;
@@ -434,6 +440,12 @@ public class MapClientService extends ProfileService {
         }
         return mapStateMachine.setMessageStatus(handle, status);
     }
+    public synchronized boolean sendImage(BluetoothDevice device, Uri[] contacts, String ImagePath,
+            PendingIntent sentIntent, PendingIntent deliveredIntent) {
+        MceStateMachine mapStateMachine = mMapInstanceMap.get(device);
+         return mapStateMachine != null
+                 && mapStateMachine.sendMapImageMessage(contacts, ImagePath, sentIntent, deliveredIntent);
+    }
 
     @Override
     public void dump(StringBuilder sb) {
@@ -578,6 +590,19 @@ public class MapClientService extends ProfileService {
                     "Need SEND_SMS permission");
 
             return service.sendMessage(device, contacts, message, sentIntent, deliveredIntent);
+        }
+
+        @Override
+        public boolean sendImage(BluetoothDevice device, Uri[] contacts, String ImagePath,
+                PendingIntent sentIntent, PendingIntent deliveredIntent, AttributionSource source) {
+            MapClientService service = getService(source);
+            if (service == null) {
+                return false;
+            }
+            if (DBG) Log.d(TAG, "Checking Permission of sendMessage");
+            mService.enforceCallingOrSelfPermission(Manifest.permission.SEND_SMS,
+                    "Need SEND_SMS permission");
+            return service.sendImage(device, contacts, ImagePath, sentIntent, deliveredIntent);
         }
 
         @Override

rd-stuffs/android_vendor_qcom-opensource_packages_apps_Bluetooth

src/com/android/bluetooth/mapclient/MapClientService.java
@@ -13,6 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ /*
+ * Changes from Qualcomm Innovation Center are provided under the following license:
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ *
+*/
 
 package com.android.bluetooth.mapclient;
 
@@ -53,7 +59,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class MapClientService extends ProfileService {
     private static final String TAG = "MapClientService";
 
-    static final boolean DBG = false;
+    static final boolean DBG = true;
     static final boolean VDBG = false;
 
     static final int MAXIMUM_CONNECTED_DEVICES = 4;
@@ -435,6 +441,12 @@ public class MapClientService extends ProfileService {
         }
         return mapStateMachine.setMessageStatus(handle, status);
     }
+    public synchronized boolean sendImage(BluetoothDevice device, Uri[] contacts, String ImagePath,
+            PendingIntent sentIntent, PendingIntent deliveredIntent) {
+        MceStateMachine mapStateMachine = mMapInstanceMap.get(device);
+         return mapStateMachine != null
+                 && mapStateMachine.sendMapImageMessage(contacts, ImagePath, sentIntent, deliveredIntent);
+    }
 
     @Override
     public void dump(StringBuilder sb) {
@@ -655,6 +667,29 @@ public class MapClientService extends ProfileService {
                     service.enforceCallingOrSelfPermission(Manifest.permission.READ_SMS,
                             "Need READ_SMS permission");
                     result = service.getUnreadMessages(device);
+                }
+                receiver.send(result);
+            } catch (RuntimeException e) {
+                receiver.propagateException(e);
+            }
+        }
+
+        @Override
+        public void sendImage(BluetoothDevice device, Uri[] contacts, String ImagePath,
+                PendingIntent sentIntent, PendingIntent deliveredIntent, AttributionSource source,
+                SynchronousResultReceiver receiver) {
+            if (VDBG) {
+                Log.v(TAG, "sendImage()");
+            }
+            try {
+                MapClientService service = getService(source);
+                boolean result = false;
+                if (service != null) {
+                    if (DBG) Log.d(TAG, "Checking Permission of sendMessage");
+                    service.enforceCallingOrSelfPermission(Manifest.permission.SEND_SMS,
+                            "Need SEND_SMS permission");
+                    result = service.sendImage(device, contacts, ImagePath, sentIntent,
+                            deliveredIntent);
                 }
                 receiver.send(result);
             } catch (RuntimeException e) {